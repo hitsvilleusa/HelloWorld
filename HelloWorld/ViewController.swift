a62fc169e5e64ae7bfb07079fd0a21ddf10dec83
Chris Morris <cmorris@bignerdranch.com>
2017-01-27 12:28:47 -0800

    Testing more recent build

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Chris Morris on 1/27/17.
+//  Copyright © 2017 HitsvilleDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
